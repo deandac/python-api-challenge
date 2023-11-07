6cbcae90fa905288d11caef7ba2683d19daa2c4e
deandac <deandac805@gmail.com>
2023-11-06 16:37:24 -0800

    final3

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e7644103fc45b86b253afdbf424b3d47"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "adab2e7a273c4b3bbc268f5e26b68676"
